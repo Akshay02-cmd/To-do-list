db22cceda330288b0ee46a47154380723754fac0
Akshay02-cmd <akshaydpatil1646@gmail.com>
2025-06-26 16:43:28 +0530

    input empty then disable the add btn

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const todoinput = document.getElementById("todoinput");
 const addbtn = document.getElementById("addbutton");
 const todolist = document.getElementById("todolist");
 
+
+if( todoinput.value == ' '){
+    addbtn.disable = true;
+}
+
 addbtn.addEventListener('click', addTodo);
 todoinput.addEventListener('keypress',function(e){
     if(e.key == 'Enter'){
